810b1386bf26185ec5ef976fd5a32b904439f152
Shreyash Joshi <90604516+Godfathxx@users.noreply.github.com>
2023-10-14 20:24:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import the CSS file for styling
 
+function WordCounter() {
+  const [state, setState] = React.useState({ 
+    charCount: 0
+  });
+  const handleKeyPress = (e) => {
+    const count = e.target.value;
+    setState({
+      charCount: count.length
+    });
+  };
+  return (
+    <div>
+  <p class="ul"><label for="w3review"><h1>Responsive Paragraph Word Counter</h1></label></p>
+  <textarea placeholder='Type here' onChange={handleKeyPress} rows="5" cols="20" >    
+    </textarea>
+    <br/>
+  <span>Word Count: {state.charCount}</span>
+  <br/>
+   </div>
+  );
+  };
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WordCounter />
   );
 }
 
 export default App;
+
